adcc2ca69f9051b2f4e6157df3269f2d9ac72a78
Hijbullah Amin <hijbullaah@gmail.com>
2020-11-06 15:09:50 +0600

    admin route move to admin.php file

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExamController;
-use App\Http\Controllers\BatchController;
-use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\Student\ResultController;
 use App\Http\Controllers\Student\StudentController;
 
@@ -38,45 +35,5 @@ Route::name('app.')->middleware(['auth:sanctum', 'verified'])->group(function ()
     });
 });
 
-
-// admin
-Route::prefix('admin')->middleware(['auth:sanctum', 'verified'])->group(function() {
-    Route::get('/dashboard', function() {
-        return Inertia\Inertia::render('Dashboard');
-    })->name('admin.dashboard');
-
-    // batch
-    Route::get('/batches', [BatchController::class, 'index'])->name('batches.index');
-    Route::get('/batches/create', [BatchController::class, 'create'])->name('batches.create');
-    Route::post('/batches', [BatchController::class, 'store'])->name('batches.store');
-    Route::get('/batches/{batch:batch_id}', [BatchController::class, 'show'])->name('batches.show');
-    Route::get('/batches/{batch:batch_id}/edit', [BatchController::class, 'edit'])->name('batches.edit');
-    Route::put('/batches/{batch:batch_id}', [BatchController::class, 'update'])->name('batches.update');
-    Route::delete('/batches/{batch:batch_id}', [BatchController::class, 'destroy'])->name('batches.destroy');
-
-    // students
-
-    Route::get('/batches/{batch}/students', [BatchController::class, 'showStudents'])->name('batches.students.index');
-
-    // exams
-    Route::get('/batches/{batch:batch_id}/exams', [ExamController::class, 'index'])->name('exams.index');
-    Route::get('/batches/{batch:batch_id}/exams/create', [ExamController::class, 'create'])->name('exams.create');
-    Route::post('/batches/{batch:batch_id}/exams', [ExamController::class, 'store'])->name('exams.store');
-    Route::get('/batches/{batch:batch_id}/exams/{exam:exam_id}', [ExamController::class, 'show'])->name('exams.show');
-    Route::put('/exams/update-basic-info/{exam:exam_id}', [ExamController::class, 'updateBasicInfo'])->name('exams.update-basic-info');
-    Route::put('/exams/update-schedule/{exam:exam_id}', [ExamController::class, 'updateExamSchedule'])->name('exams.update-schedule');
-    Route::post('/exams/enable-negetive-mark/{exam:exam_id}', [ExamController::class, 'enableNegetiveMark'])->name('exams.enable-negetive-mark');
-    Route::post('/exams/update-negetive-mark/{exam:exam_id}', [ExamController::class, 'updateNegetiveMark'])->name('exams.update-negetive-mark');
-    Route::post('/exams/publish-exam/{exam:exam_id}', [ExamController::class, 'publishExam'])->name('exams.publish');
-    Route::delete('/exams/{exam:exam_id}', [ExamController::class, 'destroy'])->name('exams.destroy');
-
-    // questions
-    Route::get('/exams/{exam:exam_id}/questions', [QuestionController::class, 'index'])->name('questions.index');
-    Route::post('/exams/{exam:exam_id}/questions', [QuestionController::class, 'store'])->name('questions.store');
-    Route::put('/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
-    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
-
-
-});
 
 
